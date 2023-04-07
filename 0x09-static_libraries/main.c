6bcc48005e88fd1f5f2d4cff8194fbea2a4577f1
jaf397 <jafermy397@gmail.com>
2023-04-07 16:19:49 -0700

    up

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,9 @@
 #include "main.h"
-
+#include <unistd.h>
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	int _puts(char *s){
+    	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
     return (0);
+	}
 }
