4a918f41f44ca790775aaa2017d1b106147ab0df
jaf397 <jafermy397@gmail.com>
2023-04-19 18:46:57 -0700

    why

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -9,7 +9,7 @@
  */
 void print_name_as_is(char *name)
 {
-    printf("Hello, my name is %s\n", name);
+	printf("Hello, my name is %s\n", name);
 }
 
 /**
@@ -20,26 +20,26 @@ void print_name_as_is(char *name)
  */
 void print_name_uppercase(char *name)
 {
-    unsigned int i;
+	unsigned int i;
 
-    printf("Hello, my uppercase name is ");
-    i = 0;
-    while (name[i])
-    {
-        if (name[i] >= 'a' && name[i] <= 'z')
-        {
-            putchar(name[i] + 'A' - 'a');
-        }
-        else
-        {
-            putchar(name[i]);
-        }
-        i++;
-    }
+	printf("Hello, my uppercase name is ");
+	i = 0;
+	while (name[i])
+	{
+		if (name[i] >= 'a' && name[i] <= 'z')
+		{
+			putchar(name[i] + 'A' - 'a');
+		}
+		else
+		{
+			putchar(name[i]);
+		}
+		i++;
+	}
 }
 
 /**
- * main - check the code
+ * main - check the code.
  *
  * Return: Always 0.
  */
